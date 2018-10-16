974cce1b9a10131bbfc6e92bde264b13110e85ae
Sebastian Olejarz <sebastian.olejarz@brickendon.com>
2018-10-16 17:59:13 +0200

    Remove tblApplicationEventLog from gitignore

---
+++ BrodzikDB/Schemas/log/Tables/tblApplicationEventLog.sql
@@ -0,0 +1,13 @@
+﻿-- ************************************** [log].[tblApplicationEventLog]
+
+CREATE TABLE [log].[tblApplicationEventLog]
+(
+ [EventID]      BIGINT IDENTITY (1, 1) NOT NULL ,
+ [EventDate]    DATETIME NOT NULL ,
+ [EventMessage] NVARCHAR(128) NOT NULL ,
+ [ErrorMessage] NVARCHAR(128) NULL ,
+ [UserID]       INT NOT NULL ,
+
+ CONSTRAINT [PK_tblApplicationEventLog] PRIMARY KEY CLUSTERED ([EventID] ASC)
+);
+GO
