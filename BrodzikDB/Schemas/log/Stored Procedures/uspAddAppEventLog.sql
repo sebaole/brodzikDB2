c8b9b577fcd7446f6399035fcba0da8b97399e41
Sebastian Olejarz <sebastian.olejarz@brickendon.com>
2018-12-03 23:44:53 +0100

    Global refactoring (TotalPrice, GrossPrice, report.uspGet... , dbo.uspLoginUser, log.uspAddAppEventLog, ForeignKeys, dict.tblCustomParam)

---
+++ BrodzikDB/Schemas/log/Stored Procedures/uspAddAppEventLog.sql
@@ -0,0 +1,55 @@
+﻿CREATE PROCEDURE [log].[uspAddAppEventLog]
+(
+	 @LoginName		NVARCHAR(16)
+	,@EventMessage	NVARCHAR(1024)
+	,@ErrorMessage	NVARCHAR(1024) = NULL
+
+)
+AS
+
+BEGIN
+
+	SET	XACT_ABORT, NOCOUNT ON
+
+	DECLARE 
+		@ReturnValue	SMALLINT = 0
+
+	BEGIN TRY
+
+		/* some extra validations here */
+		
+		BEGIN TRAN
+					
+			/* target sql statements here */
+			INSERT INTO log.tblApplicationEventLog
+			(
+				[EventDate]    
+				,[EventMessage] 
+				,[ErrorMessage]
+				,[LoginName]
+			)
+			VALUES
+			(
+				GETDATE()
+				,@EventMessage
+				,@ErrorMessage
+				,@LoginName
+			)
+		
+		COMMIT
+
+	END TRY
+	BEGIN CATCH
+
+		SET @ReturnValue = -1
+
+		IF @@TRANCOUNT > 0 ROLLBACK TRAN
+  
+		/* raise an error */
+		;THROW
+
+	END CATCH
+
+RETURN @ReturnValue
+
+END
